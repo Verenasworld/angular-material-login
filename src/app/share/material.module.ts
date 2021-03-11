de48eece4b59648e6cc4dc806ae451904532d0a5
Verenasworld <verena.felizidas@gmail.com>
2021-03-11 16:00:23 +0100

    start new project and set up my stuff

---
+++ src/app/share/material.module.ts
@@ -0,0 +1,15 @@
+import {NgModule} from "@angular/core";
+import {MatCardModule} from "@angular/material/card";
+import {MatButtonModule} from "@angular/material/button";
+import {MatInputModule} from "@angular/material/input";
+
+@NgModule({
+  declarations: [],
+  exports: [
+    MatCardModule,
+    MatButtonModule,
+    MatInputModule,
+  ]
+})
+
+export class MaterialModule{}
