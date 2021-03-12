7f98e25a4afde2b0c675b5ac0e00269c172a57a1
Verenasworld <verena.felizidas@gmail.com>
2021-03-12 15:56:06 +0100

    first steps in registaform

--- src/app/component/registrationform/registrationform.component.ts
+++ src/app/component/registrationform/registrationform.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-registrationform',
   templateUrl: './registrationform.component.html',
-  styleUrls: ['./registrationform.component.css']
+  styleUrls: ['./registrationform.component.scss']
 })
 export class RegistrationformComponent implements OnInit {
 
